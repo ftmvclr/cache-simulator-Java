b5b18a66e2a5e960bb8d5d515e88abe6d1b83d78
ftmvclr <fatimaavcilar@marun.edu.tr>
2026-05-19 10:38:35 +0300

    blueprint of the cache and main classes
    + trace&ram files

---
+++ src/Cache.java
@@ -0,0 +1,30 @@
+package src;
+
+public class Cache {
+	int s; int b; int E;
+	String data;
+	char valid;
+	enum type {L1i, L1d, L2};
+	
+	Cache(int s, int b, int E, String data){
+		this.s = s; this.b = b;
+		this.E = E;
+		this.data = data;
+	}
+	
+	void dataLoad(){
+		
+	}
+	
+	void store() {
+		
+	}
+	
+	void instLoad() {
+		
+	}
+	
+	void modify() {
+		
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,61 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class Main {
+	static int L1s; static int L2s; // how many bits to represent set count
+	static int L1b; static int L2b; // how many bits to represent block size (64 bytes would be 6 bits)
+	static int L1E; static int L2E; // number of lines per set
+	
 	public static void main(String[] args) throws IOException {
+		Scanner scanner = new Scanner(System.in);
 		Path path = Paths.get("sys3\\RAM.dat");
 		byte[] RAM = Files.readAllBytes(path);
+		System.out.println("Put them in this order:\n"
+				+ "L1s L1E L1b L2s L2E L2b");
+		L1s = scanner.nextInt(); L1E = scanner.nextInt();
+		L1b = scanner.nextInt(); L2s = scanner.nextInt();
+		L2E = scanner.nextInt(); L2b = scanner.nextInt();
+		scanner.close();
+		
+		String instructions = Files.readString(Path.of("sys3\\test_small.trace"));	
+		Scanner insScanner = new Scanner(instructions);
+		while(insScanner.hasNextLine()) {
+			String singleInstruction = insScanner.nextLine();
+			decipherInstruction(singleInstruction);
+		}
+	}
+	// op, address, size ^ op, address, size, data
+	static void decipherInstruction(String instruction) {
+		char op = instruction.charAt(0);
+		int address = Integer.parseInt(instruction.substring(2, 11));
+		short lastCommaIndex = (short)instruction.lastIndexOf(','); 
+		int size = instruction.charAt(12) - '0';
+		String data; // from index 15 to end
+		if (lastCommaIndex <= 10) {	// op, address, size | no data, L or I
+			if (op == 'L') {
+				
+			}
+			else if(op == 'I') {
+				
+			}
+			else return; // error
+		}
+		else { // op, address, size, data | S or M 
+			data = instruction.substring(15);
+			if(op == 'S') {
+				
+			}
+			else if(op == 'M') {
+				
+			}
+			else return; // error
+		}
+			
 	}
 }
+
+//L1I-hits:0 L1I-misses:1 L1I-evictions:0
+//L1D-hits:1 L1D-misses:1 L1D-evictions:0
+//L2-hits:1 L2-misses:2 L2-evictions:0
